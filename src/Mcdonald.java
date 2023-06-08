08bb0050786228c420605e951fd9c42f8d551b42
sanhoun <133223405+sanhoun@users.noreply.github.com>
2023-06-08 15:41:32 +0900

    주석처리한곳
    풀리퀘스트 에 올려 드린곳 주석 했습니다.

--- src/Mcdonald.java
+++ src/Mcdonald.java
@@ -89,7 +89,7 @@ public class Mcdonald {
                 returnMain();
         }
     }
-    private List<Menu> burgerList = new ArrayList<>();
+    //private List<Menu> burgerList = new ArrayList<>();
 
     public void burgerPrint() {
         java.lang.System.out.println("★ 맥도날드에 오신걸 환영합니다 ★");
@@ -104,7 +104,7 @@ public class Mcdonald {
             returnMain();
         }
     }
-    private List<Menu> sideList = new ArrayList<>();
+    //private List<Menu> sideList = new ArrayList<>();
 
     public void sidePrint() {
         java.lang.System.out.println("★ 맥도날드에 오신걸 환영합니다 ★");
@@ -120,7 +120,7 @@ public class Mcdonald {
             returnMain();
         }
     }
-    private List<Menu> drinkList = new ArrayList<>();
+   // private List<Menu> drinkList = new ArrayList<>();
 
     public void drinkPrint() {
         java.lang.System.out.println("★ 맥도날드에 오신걸 환영합니다 ★");
