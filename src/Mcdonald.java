b3402e5953ef0504993c33b6afec254df4adc561
boyoung0324 <132771141+boyoung0324@users.noreply.github.com>
2023-06-08 22:40:54 +0900

    Update Mcdonald.java

--- src/Mcdonald.java
+++ src/Mcdonald.java
@@ -4,28 +4,29 @@ public class Mcdonald {
 
     InitProduct ip = new InitProduct();
     Order order = new Order();
-    List<Menu> mainList = new ArrayList<>(); //(1)메인화면에서 보여질 List
-    List<Menu> mainOrderList = new ArrayList<>(); //(2)메인화면에서 보여질 List
-    List<Menu> mainManagerList = new ArrayList<>(); //(3)메인화면에서 보여질 List
+    List<Menu> mainList = new ArrayList<>(); //메인화면에서 보여질 List
+    List<Menu> mainOrderList = new ArrayList<>();
+    List<Menu> mainManagerList = new ArrayList<>();
 
     public Mcdonald() {
         init();
-    } //클래스를 생성하면, init메서드로 List들 초기화
+    }
 
     public void init() {
-        mainList.add(new Menu("햄버거", "주문 즉시 바로 조리해 더욱 맛있는, 맥도날드의 다양한 버거를 소개합니다."));
-        mainList.add(new Menu("사이드", "가볍게 즐겨도, 버거와 함께 푸짐하게 즐겨도, 언제나 맛있는 사이드!"));
-        mainList.add(new Menu("음료수", "다양한 음료를 부담없이 즐기세요!"));
+        mainList.add(new Menu("햄버거", "주문 즉시 바로 조리해 더욱 맛있는, 맥도날드의 다양한 버거를 소개합니다"));
+        mainList.add(new Menu("사이드", "가볍게 즐겨도, 버거와 함께 푸짐하게 즐겨도, 언제나 맛있는 사이드"));
+        mainList.add(new Menu("음료수", "다양한 음료를 부담없이 즐기세요"));
 
-        mainOrderList.add(new Menu("장바구니", "장바구니를 확인 후 주문합니다."));
-        mainOrderList.add(new Menu("주문취소", "진행중인 주문을 취소하고 장바구니를 비웁니다."));
-        mainOrderList.add(new Menu("주문 현황", "최근 주문목록 3건을 보여줍니다."));
+        mainOrderList.add(new Menu("장바구니", "장바구니를 확인 후 주문합니다"));
+        mainOrderList.add(new Menu("주문취소", "진행중인 주문을 취소하고 장바구니를 비웁니다"));
+        mainOrderList.add(new Menu("주문 현황", "주문 현황을 보여줍니다"));
 
         mainManagerList.add(new Menu("대기 목록", "대기 중인 주문을 보여줍니다."));
         mainManagerList.add(new Menu("완료 목록", "완료된 주문을 보여줍니다."));
         mainManagerList.add(new Menu("상품 생성", "새 상품을 등록합니다."));
         mainManagerList.add(new Menu("상품 삭제", "상품을 삭제합니다."));
     }
+
 
     public void kiosk() { //1번
         System.out.println("-------------------------------------");
@@ -39,7 +40,6 @@ public class Mcdonald {
         System.out.println("[ORDER MENU]");
         nextnum = mainPrint(nextnum, mainOrderList);
         System.out.println();
-
         System.out.println("0. MANAGER MENU로 이동");
         System.out.println("-------------------------------------");
 
@@ -75,22 +75,20 @@ public class Mcdonald {
                 cancelPrint();
                 break;
             case 6:
-                //주문현황
+                order.recentOrder();
                 break;
-
             case 0:
                 managerPrint();
                 break;
 
             default:
-                java.lang.System.out.println("해당하는 메뉴가 없습니다.");
+                System.out.println("해당하는 메뉴가 없습니다.");
                 returnMain();
-
         }
     }
 
     public void managerPrint() {
-        java.lang.System.out.println("[MANAGER MENU]");
+        System.out.println("[MANAGER MENU]");
         mainPrint(1, mainManagerList);
         System.out.println();
         System.out.println("0. MAIN MENU로 이동");
@@ -103,66 +101,63 @@ public class Mcdonald {
         int choice = sc.nextInt();
         switch (choice) {
             case 1:
-                waitPrint(); //대기목록조회
+                waitPrint();
                 break;
             case 2:
-                //완료목록조회
+                completionPrint();
                 break;
             case 3:
-               //상품생성
+                writeComplet();
                 break;
             case 4:
-               //상품삭제
+                delComplet();
                 break;
             case 0:
                 kiosk();
                 break;
 
             default:
-                java.lang.System.out.println("해당하는 메뉴가 없습니다.");
+                System.out.println("해당하는 메뉴가 없습니다.");
                 returnManagerMenu();
         }
     }
-    //private List<Menu> burgerList = new ArrayList<>();
+
+
+
 
     public void burgerPrint() {
         System.out.println("★ 맥도날드에 오신걸 환영합니다 ★");
         System.out.println("↓ 아래 메뉴판을 골라 입력해주세요.\n");
         System.out.println("[BURGER MENU]");
-
+        List<Menu> burgerList = ip.returnList("햄버거");
         if (burgerList != null) {
-            //CRUD클래스에서 생성한 상품이 보여지도록. null이 아니면 출력
-            foodPrint();//생성한 상품의 list를 매개변수로담기
+            foodPrint(burgerList);
         } else {
-            System.out.println("상품이 없습니다."); //null이면 메세지 띄움
+            System.out.println("상품이 없습니다.");
             returnMain();
         }
     }
-    //private List<Menu> sideList = new ArrayList<>();
 
     public void sidePrint() {
         System.out.println("★ 맥도날드에 오신걸 환영합니다 ★");
         System.out.println("↓ 아래 메뉴판을 골라 입력해주세요.\n");
         System.out.println("[SIDE MENU]");
-
+        List<Menu> sideList = ip.returnList("사이드");
         if (sideList != null) {
-            //CRUD클래스에서 생성한 상품이 보여지도록. null이 아니면 출력
-            foodPrint();//생성한 상품의 list를 매개변수로담기
-
+            foodPrint(sideList);
         } else {
             System.out.println("상품이 없습니다.");
             returnMain();
         }
     }
-   // private List<Menu> drinkList = new ArrayList<>();
 
     public void drinkPrint() {
         System.out.println("★ 맥도날드에 오신걸 환영합니다 ★");
         System.out.println("↓ 아래 메뉴판을 골라 입력해주세요.\n");
         System.out.println("[DRINK MENU]");
+        List<Menu> drinkList = ip.returnList("음료수");
         if (drinkList != null) {
-            //CRUD클래스에서 생성한 상품이 보여지도록. null이 아니면 출력
-            foodPrint();//생성한 상품의 list를 매개변수로담기
+            foodPrint(drinkList);
         } else {
             System.out.println("상품이 없습니다.");
             returnMain();
@@ -171,19 +166,19 @@ public class Mcdonald {
     }
 
 
-    void foodPrint(List<Menu> foodList) { //상품 프린트되는 공통 메서드
+    void foodPrint(List<Menu> foodList) { //음식 프린트 공동 사용
         for (int i = 0; i < foodList.size(); i++) {
             System.out.printf("%d. %s | %d원 | %s\n", i + 1, foodList.get(i).name, foodList.get(i).price, foodList.get(i).desc);
         }
         foodChoice(foodList);
     }
 
-    void foodChoice(List<Menu> foodList) { //상품 선택하는 공동 메서드
+    void foodChoice(List<Menu> foodList) { //음식 선택 메서드 공동 사용
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
-        if (choice >= 1 && choice <= foodList.size()) { //상품 사이즈내에서 선택하도록 (ex_1.상하이 2.빅맥 3.슈슈버거 )
-            Menu menu = foodList.get(choice - 1); // (ex_(1)내가 1번 상하이를 선택함 -> 상하이는 0번째 인덱스에 위치 -> 상품번호에서 -1하면 인덱스랑 맞춰짐 ->상하이 객체를 꺼내서 Menu로 받음
-            orderCart(menu); //(2)상하이 객체를 장바구니에 넣기
+        if (choice >= 1 && choice <= foodList.size()) { //메뉴번호 1,2,3,4
+            Menu menu = foodList.get(choice - 1); // 메뉴번호에서 -1하면 인덱스랑 맞춰짐
+            orderCart(menu);
         } else {
             System.out.println("해당하는 상품이 없습니다.");
             returnMain();
@@ -191,19 +186,19 @@ public class Mcdonald {
 
     }
 
-    void orderCart(Menu menu) { //1.장바구니 추가하는 공통 메서드
-        System.out.printf("[%s | %d원 | %s]\n", menu.getName(), menu.getPrice(), menu.getDesc()); //위에서 받은 Menu객체 꺼내기
+    void orderCart(Menu menu) { //공통 메서드2
+        System.out.printf("[%s | %d원 | %s]\n", menu.getName(), menu.getPrice(), menu.getDesc());
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.println("1. 확인\t 2. 취소");
         cartInput(menu);
     }
 
 
-    void cartInput(Menu menu) { //2.장바구니 추가하는 공통 메서드
+    void cartInput(Menu menu) { //공통 메서드 3
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
         if (choice == 1) {
-            order.cartAdd(menu); //장바구니List에 담는 메서드
+            order.cartAdd(menu);
             System.out.println("장바구니에 추가되었습니다.");
             kiosk();
         } else if (choice == 2) {
@@ -214,9 +209,22 @@ public class Mcdonald {
         }
     }
 
+    void writeComplet() { //상품등록 완료
+        ip.write();
+        System.out.println();
+        System.out.println("상품이 등록되었습니다. ");
+        returnManagerMenu();
+    }
 
-    public void cartList() { //(1)장바구니 상품들 주문하는 메서드
-        if (order.orderList.size() != 0) { //장바구니 List에 상품이 있으면,출력
+    void delComplet() { //지우기 완료
+        ip.delete();
+
+        returnManagerMenu();
+    }
+
+
+    public void cartList() {
+        if (order.orderList.size() != 0) {
 
             System.out.println("[Order]");
             order.orderListPrint();
@@ -236,7 +244,7 @@ public class Mcdonald {
 
     }
 
-    void orderInput() { //(2)장바구니 상품들 주문하는 메서드
+    void orderInput() {
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
         if (choice == 1) {
@@ -259,6 +267,52 @@ public class Mcdonald {
         kiosk();
     }
 
+    public void returnMain() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("0. 메인으로 돌아가기");
+        int choice = sc.nextInt();
+        if (choice == 0) {
+            kiosk();
+        } else {
+            returnMain();
+        }
+    }
+
+    public void returnManagerMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("0. 관리메뉴로 돌아가기");
+        int choice = sc.nextInt();
+        if (choice == 0) {
+            managerPrint();
+        } else {
+            returnManagerMenu();
+        }
+    }
+
+    public void waitPrint() { //대기 상품 목록
+        if (order.waitList.size() != 0) {
+            order.waitListPrint();
+            System.out.println();
+            System.out.println("완료되었습니다.");
+            returnManagerMenu();
+        } else {
+            System.out.println("[ 대기중인 상품이 없습니다. ]");
+            returnManagerMenu();
+        }
+    }
+
+    public void completionPrint() { //완료 상품 목록
+        if (order.compList.size() != 0) {
+            order.compListPrint();
+            returnManagerMenu();
+        } else {
+            System.out.println("[ 완료된 상품이 없습니다. ]");
+            returnManagerMenu();
+        }
+    }
+
     public void cancelPrint() {
         Scanner sc = new Scanner(System.in);
         System.out.println("진행하던 주문을 취소하시겠습니까?");
@@ -273,39 +327,4 @@ public class Mcdonald {
             kiosk();
         }
     }
-
-    public void waitPrint() { //대기 상품 목록
-        if (order.waitList.size() != 0) {
-            order.waitListPrint();
-            java.lang.System.out.println();
-            java.lang.System.out.println("완료되었습니다.");
-            returnManagerMenu();
-        } else {
-            java.lang.System.out.println("[ 대기중인 상품이 없습니다. ]");
-            returnManagerMenu();
-        }
-    }
-
-    public void returnMain() { //메인으로 돌아가는 메서드. 여기저기 끝에 붙여주면 좋음
-        Scanner sc = new Scanner(System.in);
-        System.out.println();
-        System.out.println("0. 메인으로 돌아가기");
-        int choice = sc.nextInt();
-        if (choice == 0) {
-            kiosk();
-        }
-    }
-    public void returnManagerMenu() {
-        Scanner sc = new Scanner(java.lang.System.in);
-        java.lang.System.out.println();
-        java.lang.System.out.println("0. 관리메뉴로 돌아가기");
-        int choice = sc.nextInt();
-        if (choice == 0) {
-            managerPrint();
-        } else {
-            returnManagerMenu();
-        }
-    }
-
-
 }
